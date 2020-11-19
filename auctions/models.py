fc09bbbfb9229aeca65f862dbcd801426709030d
CarinaLR <carinalr84@gmail.com>
2020-11-19 10:11:39 -0500

    first commit

---
+++ auctions/models.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+    price = models.FloatField()
+
+    def _str_(self):
+        return self.name
+
+
+class OrderProduct(models.Model):
+    user = models.ForeignKey(
+        User,  default=1, on_delete=models.CASCADE, related_name="list")
+    ordered = models.BooleanField(default=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def serialize(self):
+        return {
+            "username": self.user,
+            "ordered": self.ordered,
+            "product": self.product,
+            "quantity": self.quantity,
+        }
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE, related_name='users')
+    products = models.ManyToManyField(OrderProduct)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self, user.username
+
+    def get_order(self):
+        total = 0
+        for order_product in self.products.all():
+            total += order_product
+        return total
