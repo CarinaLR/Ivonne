679475ceb3f78c959ce2ea33e348c00fb01635e2
CarinaLR <carinalr84@gmail.com>
2021-01-28 16:45:37 -0500

    added static root

--- commerce/settings.py
+++ commerce/settings.py
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra lookup directories for collectstatic to find static files
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'auctions/static/'),)
