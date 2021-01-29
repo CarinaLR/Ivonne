d68665360d142d80d24fbc008b1418a92f9b1369
CarinaLR <carinalr84@gmail.com>
2021-01-29 09:25:25 -0500

    added allowed hosts

--- commerce/settings.py
+++ commerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0',
-                 'localhost', '127.0.0.1']
+                 'juzmooth-ec.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
