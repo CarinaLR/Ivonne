bc954864eac3f0ccc2dd898562f2707979e814fc
CarinaLR <carinalr84@gmail.com>
2021-01-29 10:00:10 -0500

    added database

--- commerce/settings.py
+++ commerce/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'HOST': os.getenv('DATABASE_HOSTNAME'),
+        'PORT': 5432,
+        'USER': os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
     }
 }
 
